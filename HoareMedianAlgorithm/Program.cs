ed6776111d88ca5b7af0b600f2d53aa40cd8b063
pavel Švec <pavel.svec@gmail.com>
2022-11-08 15:20:10 +0100

    Bugfix

--- HoareMedianAlgorithm/Program.cs
+++ HoareMedianAlgorithm/Program.cs
@@ -4,18 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int[] arr = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17, 3 };
+            int[] arr = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17 };
+            int[] cpy;
 
-            int valueOnNthPosition = Hoare(arr, 5);
-
-            Console.WriteLine(valueOnNthPosition);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int valueOnNthPosition = Hoare((int[])arr.Clone(), i);
+                Console.WriteLine(valueOnNthPosition);
+            }
         }
 
         private static int Hoare(int[] arr, int v)
         {
-            int l=1, r=arr.Length-1, i, j, w, x;
+            int l=0, r=arr.Length-1, i=0, j, w, x;
 
-            while(l < r)
+            Console.WriteLine(string.Join(", ", arr));
+
+            while (l < r)
             {
                 x = arr[v];
                 i = l;
@@ -38,16 +43,23 @@
                         arr[j] = w;
                         i++;
                         j--;
+                        Console.WriteLine(string.Join(", ", arr));
                     }
-                } while (i > j);
+                } while (i < j);
+                Console.ReadLine();
 
                 if (j < v)
                     l = i;
                 if (v < i)
                     r = j;
+                if (i == j)
+                {
+                    Console.WriteLine($"{v}. v pořadí je na indexu {i}");
+                    Console.ReadLine();
+                }
             }
 
-            return arr[v];
+            return arr[i];
         }
     }
 }
