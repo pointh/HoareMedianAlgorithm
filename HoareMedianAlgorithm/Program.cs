df7bd52da14ab37c405f4f6e64fe753639aa7959
Pavel Svec <pavel.svec@itcrawl.cz>
2022-11-09 12:44:28 +0100

    Doladěno a komentáře k výpočtu, porovnání se sortováním.

--- HoareMedianAlgorithm/Program.cs
+++ HoareMedianAlgorithm/Program.cs
@@ -4,62 +4,97 @@
     {
         static void Main(string[] args)
         {
-            int[] arr = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17 };
-            int[] cpy;
+            int i = 0;
+            int[] arr = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17, 3, 7 };
 
-            for (int i = 0; i < arr.Length; i++)
+#if DEBUG
+            int arrSize = 0;
+            do
             {
-                int valueOnNthPosition = Hoare((int[])arr.Clone(), i);
-                Console.WriteLine(valueOnNthPosition);
+                int[] arrRefreshed = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17, 3, 7};
+                if (i == 0)
+                {
+                    arrSize = arrRefreshed.Length;
+                }
+                
+                Console.WriteLine(String.Join(",\t", Enumerable.Range(0, arrSize).Select(x => x)));
+                Console.WriteLine("".PadLeft(8*arrSize, '-'));
+                Console.WriteLine(string.Join(",\t", arrRefreshed));
+
+                int valueOnNthPosition = HoareNthLowesElement(arrRefreshed, i);
+                Console.WriteLine($"{1+i++}. v pořadí je {valueOnNthPosition}\n");
             }
+            while (i < arrSize);
+#endif
+
+            Console.WriteLine($"Medián prvního pole pole je {HoareNthLowesElement(arr, arr.Length/2)}\n");
+
+            int[] arr2 = new int[10_000_000];
+            Random r = new Random();
+            for (int k = 0; k<arr2.Length; k++)
+            {
+                arr2[k] = r.Next();
+            }
+
+            Console.WriteLine($"{DateTime.Now.Second}:{DateTime.Now.Millisecond}");
+            Console.WriteLine($"Medián druhého pole pole je {HoareNthLowesElement(arr2, arr2.Length / 2)}\n");
+
+            //Array.Sort(arr2);
+            //Console.WriteLine($"Medián druhého pole pole je {arr2[arr2.Length / 2]}\n");
+
+            Console.WriteLine($"{DateTime.Now.Second}:{DateTime.Now.Millisecond}");
+
         }
 
-        private static int Hoare(int[] arr, int v)
+        private static int HoareNthLowesElement(int[] data, int n)
         {
-            int l=0, r=arr.Length-1, i=0, j, w, x;
+            int left = 0, right = data.Length - 1;
+            int idxLeft = left, idxRight;
+            int temp, x;
 
-            Console.WriteLine(string.Join(", ", arr));
-
-            while (l < r)
+            while (left < right)
             {
-                x = arr[v];
-                i = l;
-                j = r;
+                x = data[n];  // prvek aktuálně na hledaném pořadí
+                idxLeft = left;
+                idxRight = right;
                 do
                 {
-                    while (arr[i] < x)
+                    while (data[idxLeft] < x) // zvyšuj index zleva, dokud jsou vlevo prvky menší než x
                     {
-                        i++;
+                        idxLeft++;
                     }
-                    while (arr[j] > x)
+                    while (data[idxRight] > x)// snižuj index zprava, dokud jsou prvky vpravo od x větší než x
                     {
-                        j--;
+                        idxRight--; 
                     }
 
-                    if (i < j)
+                    // tady idxLeft ukazuje na první prvek v data[idxLeft], který je větší než x
+                    // a současně
+                    // idxRight ukazuje na první prvek v data[indexRight], který je menší než x 
+                    // idxLeft a idxRight ukazují tedy na prvky, které nejsou na správném místě z hlediska vztahu k x
+                    // Všechny prvky na indexech pod x musejí být menší než x a 
+                    // vechny prvky na indexex nad x musejí být větší než x.
+                    
+                    // Proto je třeba prvky prohodit:
+                    if (idxLeft <= idxRight) 
                     {
-                        w = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = w;
-                        i++;
-                        j--;
-                        Console.WriteLine(string.Join(", ", arr));
+                        temp = data[idxLeft];   // prohoď prvky
+                        data[idxLeft] = data[idxRight];
+                        data[idxRight] = temp;
+                        idxLeft++;              // a posuň se v obou směrech (prvky na idxLeft a idxRight jsou už v pořádku 
+                        idxRight--;
+#if DEBUG
+                        Console.WriteLine(string.Join(",\t", data));
+#endif
                     }
-                } while (i < j);
-                Console.ReadLine();
+                } while (idxLeft < idxRight);
 
-                if (j < v)
-                    l = i;
-                if (v < i)
-                    r = j;
-                if (i == j)
-                {
-                    Console.WriteLine($"{v}. v pořadí je na indexu {i}");
-                    Console.ReadLine();
-                }
+                if (idxRight < n)  // příliš mnoho prvků je za prvkem data[n]
+                    left = idxLeft;  // hledáme v intervalu napravo od data[n]
+                if (idxLeft > n)   // příliš mnoho prvků je před prvkem data[n]
+                    right = idxRight; // hledáme v interval nalevo od data[n]
             }
-
-            return arr[i];
+            return data[n];
         }
     }
 }
