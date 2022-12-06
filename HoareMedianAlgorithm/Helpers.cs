ff6259042ccad58105ca7f851ed94db977ca4561
Pavel Svec <pavel.svec@itcrawl.cz>
2022-12-06 10:50:11 +0100

    Změna ve výpisu pořadí

---
+++ HoareMedianAlgorithm/Helpers.cs
@@ -0,0 +1,47 @@
+﻿namespace HoareMedianAlgorithm
+{
+    internal class Helpers
+    {
+        public static void ShowActualArray(int[] data, int leftIndex, int rightIndex, int spanLeft, int spanRight, int order, string comment)
+        {
+            const int posWidth = 5;
+
+            char[] indexLabel = "".PadRight(posWidth * (data.Length + 2 + posWidth + comment.Length)).ToCharArray();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (i == order)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+
+                if (i == leftIndex) { Console.ForegroundColor = ConsoleColor.Yellow; }
+                if (i == rightIndex) { Console.ForegroundColor = ConsoleColor.Yellow; }
+
+                if (i >= spanLeft && i <= spanRight) { Console.BackgroundColor = ConsoleColor.DarkGray; }
+                //if (i <= spanRight) { Console.BackgroundColor = ConsoleColor.DarkRed; }
+
+                Console.Write($"{data[i],posWidth}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine();
+
+            //indexLabel[(posWidth) * leftIndex + posWidth - 1] = '^';
+            string leftIndexLabel = $"{leftIndex}^";
+            leftIndexLabel.CopyTo(0, indexLabel, posWidth * leftIndex + posWidth - 2, leftIndexLabel.Length);
+
+            //indexLabel[(posWidth) * leftIndex + 3] = $"^{leftIndex}"[0];
+            //indexLabel[(posWidth) * rightIndex + posWidth - 1] = '|';
+
+            string rightIndexLabel = $"|{rightIndex}";
+            rightIndexLabel.CopyTo(0, indexLabel, posWidth * rightIndex + posWidth - 1, rightIndexLabel.Length);
+
+            comment.CopyTo(0, indexLabel, data.Length * posWidth + 10, comment.Length);
+
+            Console.WriteLine(indexLabel);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
--- HoareMedianAlgorithm/Program.cs
+++ HoareMedianAlgorithm/Program.cs
@@ -1,72 +1,47 @@
-﻿namespace HoareMedianAlgorithm
+﻿using System.Reflection;
+
+namespace HoareMedianAlgorithm
 {
-    internal class Program
+    internal class Program : Hoare2
     {
         static void Main(string[] args)
         {
             int i = 0;
-            int[] arr = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17, 3, 7 };
+            int[] arr = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17, 3, 7, 1 };
+            int searchPosition = 3;
 
-#if DEBUG
-            int arrSize = 0;
-            do
-            {
-                int[] arrRefreshed = { 10, 21, 33, 14, 5, 6, 8, 3, 12, 21, 17, 3, 7};
-                if (i == 0)
-                {
-                    arrSize = arrRefreshed.Length;
-                }
-                
-                Console.WriteLine(String.Join(",\t", Enumerable.Range(0, arrSize).Select(x => x)));
-                Console.WriteLine("".PadLeft(8*arrSize, '-'));
-                Console.WriteLine(string.Join(",\t", arrRefreshed));
-
-                int valueOnNthPosition = HoareNthLowesElement(arrRefreshed, i);
-                Console.WriteLine($"{1+i++}. v pořadí je {valueOnNthPosition}\n");
-            }
-            while (i < arrSize);
-#endif
-
-            Console.WriteLine($"Medián prvního pole pole je {HoareNthLowesElement(arr, arr.Length/2)}\n");
-
-            int[] arr2 = new int[10_000_000];
-            Random r = new Random();
-            for (int k = 0; k<arr2.Length; k++)
-            {
-                arr2[k] = r.Next();
-            }
-
-            Console.WriteLine($"{DateTime.Now.Second}:{DateTime.Now.Millisecond}");
-            Console.WriteLine($"Medián druhého pole pole je {HoareNthLowesElement(arr2, arr2.Length / 2)}\n");
-
-            //Array.Sort(arr2);
-            //Console.WriteLine($"Medián druhého pole pole je {arr2[arr2.Length / 2]}\n");
-
-            Console.WriteLine($"{DateTime.Now.Second}:{DateTime.Now.Millisecond}");
-
+            Helpers.ShowActualArray(arr, 0, arr.Length - 1, 0, arr.Length - 1, searchPosition-1, "start");
+            Console.WriteLine($"{searchPosition}. v poli pole je {HoareNthLowestElement(arr, searchPosition-1)}\n");
         }
 
-        private static int HoareNthLowesElement(int[] data, int n)
+        private static int HoareNthLowestElement(int[] data, int n)
         {
             int left = 0, right = data.Length - 1;
-            int idxLeft = left, idxRight;
+            int idxLeft = left, idxRight = right;
             int temp, x;
 
             while (left < right)
             {
+                //ShowActualArray(data, idxLeft, idxRight, left, right, n);
+
                 x = data[n];  // prvek aktuálně na hledaném pořadí
                 idxLeft = left;
                 idxRight = right;
                 do
                 {
+
                     while (data[idxLeft] < x) // zvyšuj index zleva, dokud jsou vlevo prvky menší než x
                     {
                         idxLeft++;
+                        //Helpers.ShowActualArray(data, idxLeft, idxRight, left, right, n, "posun zleva");
                     }
+
                     while (data[idxRight] > x)// snižuj index zprava, dokud jsou prvky vpravo od x větší než x
                     {
-                        idxRight--; 
+                        idxRight--;
+                        //Helpers.ShowActualArray(data, idxLeft, idxRight, left, right, n, "posun zprava");
                     }
+
 
                     // tady idxLeft ukazuje na první prvek v data[idxLeft], který je větší než x
                     // a současně
@@ -74,25 +49,29 @@
                     // idxLeft a idxRight ukazují tedy na prvky, které nejsou na správném místě z hlediska vztahu k x
                     // Všechny prvky na indexech pod x musejí být menší než x a 
                     // vechny prvky na indexex nad x musejí být větší než x.
-                    
+
                     // Proto je třeba prvky prohodit:
-                    if (idxLeft <= idxRight) 
+                    if (idxLeft <= idxRight)
                     {
                         temp = data[idxLeft];   // prohoď prvky
                         data[idxLeft] = data[idxRight];
                         data[idxRight] = temp;
                         idxLeft++;              // a posuň se v obou směrech (prvky na idxLeft a idxRight jsou už v pořádku 
                         idxRight--;
-#if DEBUG
-                        Console.WriteLine(string.Join(",\t", data));
-#endif
+
+                        Helpers.ShowActualArray(data, idxLeft, idxRight, left, right, n, "<---------->");
+
                     }
-                } while (idxLeft < idxRight);
+                } while (idxLeft < idxRight); // dokud levý index idxLeft nepřeběhl nebo nedoběhl pravý index idxRight
+
+                Helpers.ShowActualArray(data, idxLeft, idxRight, left, right, n, "překrytí indexů");
 
                 if (idxRight < n)  // příliš mnoho prvků je za prvkem data[n]
                     left = idxLeft;  // hledáme v intervalu napravo od data[n]
                 if (idxLeft > n)   // příliš mnoho prvků je před prvkem data[n]
                     right = idxRight; // hledáme v interval nalevo od data[n]
+
+                Helpers.ShowActualArray(data, idxLeft, idxRight, left, right, n, "změna mezí");
             }
             return data[n];
         }
